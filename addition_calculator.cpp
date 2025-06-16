e53198481c72e724166233dc2a35694b6e0ef185
MAHIK IRSHAD BHAT <bhatmahik.mb97@gmail.com>
2025-06-16 12:25:38 +0530

    Add files via upload

---
+++ addition_calculator.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+    int main () {
+
+        // Simple calculator which performs addition only.
+
+        double x,y;
+
+        cout << "Enter first number: ";
+        cin >> x;
+
+        cout << "Enter second number: ";
+        cin >> y;
+
+        cout << "The sum of the two numbers is: " << x + y;
+
+        return 0;
+    }
